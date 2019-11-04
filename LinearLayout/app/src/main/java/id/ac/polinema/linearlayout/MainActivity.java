713ec6795721370b38ba65c6931550ef38afe7a4
BrilianFajar <fajar28021999@gmail.com>
2019-11-04 09:54:26 +0700

    jobsheet3 (Linearlayout)

--- LinearLayout/app/src/main/java/id/ac/polinema/linearlayout/MainActivity.java
+++ LinearLayout/app/src/main/java/id/ac/polinema/linearlayout/MainActivity.java
@@ -1,8 +1,8 @@
 package id.ac.polinema.linearlayout;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
